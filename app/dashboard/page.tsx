eea56d0732675df2cccef164899b10dbe38cddae
Michael Owuor <michael.owuor@gmail.com>
2023-02-27 20:30:48 +0300

    feat: create personal dashboard (#8)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function Dashboard() {
-	return <div>Dashboard</div>;
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../../pages/api/auth/[...nextauth]';
+import PersonalPosts from './personal-posts';
+
+export default async function Dashboard() {
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		redirect('/api/auth/signin');
+	}
+
+	return (
+		<main>
+			<h1 className="text-2xl font-bold">Welcome back {session?.user?.name}</h1>
+			<PersonalPosts />
+		</main>
+	);
 }
